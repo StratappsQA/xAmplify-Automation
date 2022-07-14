cc4b901ce027a3d96af0b79da876270fc860ccb6
chmounika <chmounika@chmounika-sa>
2022-07-14 18:16:24 +0530

    Survey Campaign launched Successfully

--- src/main/java/com/xamplify/automation/SurveyCampaign.java
+++ src/main/java/com/xamplify/automation/SurveyCampaign.java
@@ -9,6 +9,8 @@ import java.util.Properties;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -47,7 +49,11 @@ public void scampaign() throws InterruptedException, SQLException {
 	
 	Thread.sleep(5000);
 	
-	WebDriverWait waitc = new WebDriverWait(driver, 40);
+ 	JavascriptExecutor js = (JavascriptExecutor) driver;
+	 js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+	Thread.sleep(5000);
+	
+	WebDriverWait waitc = new WebDriverWait(driver, 50);
 	
 	WebElement openscamp = waitc.until(
 			ExpectedConditions.visibilityOfElementLocated(By.xpath(properties.getProperty("survey_opencampaign")))); //select survey campaign
@@ -89,9 +95,8 @@ logger.info("Select ON");
 logger.info("Click on Next button");
 	driver.findElement(By.xpath(properties.getProperty("s_next"))).click();
 	Thread.sleep(4000);
-	logger.info("Goto Partner section");	
+logger.info("Goto Partner section");	
 	driver.findElement(By.xpath(properties.getProperty("p_gototop"))).click();
- 	
  	
  	WebDriverWait wait_dropdown = new WebDriverWait(driver, 50);
 	WebElement w_dropdown = wait_dropdown
@@ -99,14 +104,13 @@ logger.info("Click on Next button");
 	Thread.sleep(2000);
 	
  	Select pdropdown = new Select(w_dropdown); 
- 	pdropdown.selectByVisibleText("Count(DESC)");  //select count desc
+ 	pdropdown.selectByVisibleText("Count(ASC)");  //select count Asc
  	
  	Thread.sleep(4000);
 
 
-
 logger.info("give the invalid text");
-	driver.findElement(By.xpath(properties.getProperty("s_search_data"))).sendKeys("mnmnmnmnmn");
+	driver.findElement(By.xpath(properties.getProperty("s_search_data"))).sendKeys("mnmnmnmnmn"); // give invalid text
 	Thread.sleep(3000);
 	driver.findElement(By.xpath(properties.getProperty("s_search_click"))).click();
 	Thread.sleep(3000);
@@ -114,21 +118,40 @@ logger.info("give the invalid text");
 	Thread.sleep(3000);
 logger.info("Search for active Master partner list");
 
-	driver.findElement(By.xpath(properties.getProperty("s_search_Valid_data"))).sendKeys("Active Master Partner list");
+	driver.findElement(By.xpath(properties.getProperty("s_search_Valid_data"))).sendKeys("Active Master Partner list"); // give Valid text
 	Thread.sleep(3000);
-	driver.findElement(By.xpath(properties.getProperty("s_searchButton_click"))).click();
+	driver.findElement(By.xpath(properties.getProperty("s_searchButton_click"))).click(); //click on Search button
 	Thread.sleep(3000);
-	driver.findElement(By.xpath(properties.getProperty("s_select_partnerlist"))).click();
+	driver.findElement(By.xpath(properties.getProperty("s_select_partnerlist"))).click(); // Slect the Partner list
 	Thread.sleep(3000);
-logger.info("click on preview ");
-	driver.findElement(By.xpath(properties.getProperty("s_preview_partner_list"))).click();
+logger.info("click on preview_Partner_list ");
+	driver.findElement(By.xpath(properties.getProperty("s_preview_partner_list"))).click(); //click on Preview Icon
 	Thread.sleep(3000);
 	driver.findElement(By.xpath(properties.getProperty("s_close_preview_partner_list"))).click();
 	Thread.sleep(3000);
-	driver.findElement(By.xpath(properties.getProperty("s_next1"))).click();
+	driver.findElement(By.xpath(properties.getProperty("s_next1"))).click(); // click on Next
 	Thread.sleep(3000);
 	
+logger.info("Goto Email_Template_Section");
+	WebElement s_search=driver.findElement(By.xpath(properties.getProperty("s_search_template")));//search bar
+	s_search.sendKeys("Survey");
+	s_search.sendKeys(Keys.ENTER);
+	Thread.sleep(4000);
+
+	driver.findElement(By.xpath(properties.getProperty("s_select_temp"))).click(); // Select template
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("s_template_preview"))).click(); // click on Preview icon
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("s_template_preview_close"))).click(); 
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("s_template_edit"))).click();  // click on Edit template Icon
+	Thread.sleep(4000);
+	driver.findElement(By.xpath(properties.getProperty("s_template_edit_close"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("s_next2"))).click(); // click on Next
+	Thread.sleep(3000);
 	
+logger.info("Goto Launch_Section");
 	
 }
 	
