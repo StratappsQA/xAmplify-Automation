40ca2836600c43f924686aa1e4a1385fc4592022
chmounika <chmounika@chmounika-sa>
2022-08-02 19:01:57 +0530

    Manage Survey Campaign

--- src/main/java/com/xamplify/automation/ManageSurveyCampaign.java
+++ src/main/java/com/xamplify/automation/ManageSurveyCampaign.java
@@ -6,9 +6,12 @@ import java.util.Properties;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
@@ -24,9 +27,77 @@ final Logger logger = LogManager.getLogger(SurveyCampaign.class);
 		
 	WebDriverWait wait = new WebDriverWait(driver, 50);  // Wait till the element is not visible
 
-	WebElement scamp = wait.until(
+	WebElement mscamp = wait.until(
 			ExpectedConditions.visibilityOfElementLocated(By.xpath(properties.getProperty("campaignhover"))));
-	scamp.click(); // hover on campaign
+	mscamp.click(); // hover on campaign
+	
+	Actions mcamp_action = new Actions(driver);
+	mcamp_action.moveToElement(mscamp).perform();
+	Thread.sleep(5000);
+	WebElement manage_campele = driver.findElement(By.xpath(properties.getProperty("managecampaigns"))); //click on manage campaign
+	mcamp_action.moveToElement(manage_campele);
+	mcamp_action.click();
+	mcamp_action.perform();
 
+	Thread.sleep(4000);
+
+	
+//	driver.findElement(By.xpath(properties.getProperty("survey_tab"))).click(); //Click on Survey Tab
+//	Thread.sleep(3000);
+logger.info("Clicked on the Survey tab");
+	driver.findElement(By.xpath(properties.getProperty("gear_icon"))).click(); //Click on gear icon
+	Thread.sleep(3000);
+logger.info("Click on the Gear icon");
+	driver.findElement(By.xpath(properties.getProperty("edit"))).click();
+	Thread.sleep(3000);
+logger.info("Click on the Edit Icon");
+ 	WebDriverWait folder_dropdown = new WebDriverWait(driver, 50);
+	WebElement f_dropdown = folder_dropdown
+			.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(properties.getProperty("s_select_folder_dropdown"))));  //select folder dropdown
+	Thread.sleep(2000);
+	
+ 	Select fdropdown = new Select(f_dropdown); 
+ 	fdropdown.selectByValue("1106");  //select by value
+ 	
+ 	Thread.sleep(4000);
+ 	driver.findElement(By.xpath(properties.getProperty("update_button"))).click();//click on update
+ 	Thread.sleep(2000);
+ 	driver.findElement(By.xpath(properties.getProperty("click_close"))).click();//click on close button
+ 	Thread.sleep(4000);
+ logger.info("click on Gear icon");
+ 	driver.findElement(By.xpath(properties.getProperty("gear_icon"))).click(); //Click on gear icon
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("copy_campaign"))).click();//Click on Copy Campaign
+	Thread.sleep(2000);
+	driver.findElement(By.xpath(properties.getProperty("click_save_changes"))).click();//click on Save Changes.
+	Thread.sleep(2000);
+logger.info("campaign copied successfully");
+
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+	js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+	Thread.sleep(5000);
+	driver.findElement(By.xpath(properties.getProperty("gear_icon_t"))).click(); //Click on gear icon
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("update_end_date"))).click(); //click on update end date
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("campaign_end_date"))).click();
+	Thread.sleep(3000);
+	//driver.findElement(By.xpath(properties.getProperty("select_date"))).click();
+	//Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("save_changes"))).click();
+	Thread.sleep(3000);
+	
+//	JavascriptExecutor js1 = (JavascriptExecutor) driver;
+//	js1.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+//	Thread.sleep(5000);
+	driver.findElement(By.xpath(properties.getProperty("gear_icon_t"))).click(); //Click on gear icon
+	Thread.sleep(3000);
+	//driver.findElement(By.xpath(properties.getProperty("preview&delete_partner"))).click();
+	//Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("preview"))).click();
+	Thread.sleep(3000);
+	
 }
+
+
 }
