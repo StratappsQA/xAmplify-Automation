9ccae8d2d6ff2259a73aca23b1e59f5687730537
chmounika <chmounika@chmounika-sa>
2022-08-18 19:05:46 +0530

    Tiles under Campaign based reports

--- src/main/java/com/xamplify/automation/ManageSurveyCampaign.java
+++ src/main/java/com/xamplify/automation/ManageSurveyCampaign.java
@@ -212,22 +212,306 @@ logger.info("campaign copied successfully");
 	driver.findElement(By.xpath(properties.getProperty("cross_button"))).click();
 	Thread.sleep(4000);
 	
-	driver.findElement(By.xpath(properties.getProperty("list_view"))).click();
+	driver.findElement(By.xpath(properties.getProperty("list_view"))).click(); //Click on List View
 	Thread.sleep(5000);
-	driver.findElement(By.xpath(properties.getProperty("analytics_icon_click"))).click();
+	
+logger.info("Click on the campaign based reports");
+	driver.findElement(By.xpath(properties.getProperty("analytics_icon_click"))).click(); // Click on Campaign based Reports
 	Thread.sleep(3000);
+logger.info("Click on the Recepients");
+	
+
+	JavascriptExecutor jse = (JavascriptExecutor) driver;
+	jse.executeScript("document.getElementsByClassName('fa fa-user')");
+	Thread.sleep(3000);
+	
 	driver.findElement(By.xpath(properties.getProperty("recepients_click"))).click();
-	Thread.sleep(4000);
+	Thread.sleep(3000);
 	driver.findElement(By.xpath(properties.getProperty("total_recepients_search_box"))).sendKeys("mounika");
-	Thread.sleep(4000);
+	Thread.sleep(3000);
 	driver.findElement(By.xpath(properties.getProperty("total_recepients_search"))).click();
-	Thread.sleep(4000);
+	Thread.sleep(3000);
 	driver.findElement(By.xpath(properties.getProperty("export_excel_icon_click"))).click();
-	Thread.sleep(4000);
+	Thread.sleep(3000);
 	driver.findElement(By.xpath(properties.getProperty("export_excel_click"))).click();
-	Thread.sleep(4000);
+	Thread.sleep(3000);
 	driver.findElement(By.xpath(properties.getProperty("total_recepients_cross_click"))).click();
-	Thread.sleep(4000);
+	Thread.sleep(3000);
+	
+	driver.findElement(By.xpath(properties.getProperty("total_email_sent"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("total_email_search_box"))).sendKeys("mounika");
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("total_emailsent_search"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("export_excel_icon_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("export_excel_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("total_emailsent_cross_click"))).click();
+	Thread.sleep(3000);
+	
+	driver.findElement(By.xpath(properties.getProperty("deliverability"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("delivered_email_search_box"))).sendKeys("mounika");
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("delivered_email_search"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("export_excel_icon_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("export_excel_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("delivered_cross_click"))).click();
+	Thread.sleep(3000);
+
+	
+logger.info("Click on the Open_rate");	
+	
+	WebElement openrat = driver.findElement(By.xpath(properties.getProperty("openrate")));
+	if(openrat.isEnabled())
+	{
+	openrat.click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("emails_opened_by_recepients_search_box"))).sendKeys("automated");
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("emails_opened_by_recepients_search"))).click();
+	Thread.sleep(3000);
+	
+	WebDriverWait wait_time_drdn = new WebDriverWait(driver, 50);
+	WebElement w_time = wait_time_drdn
+			.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(properties.getProperty("time_dropdown"))));  //select dropdown
+	Thread.sleep(2000);
+	
+	Select w_t1 = new Select(w_time);
+	w_t1.selectByValue("1: Object");
+	Thread.sleep(2000);
+	w_t1.selectByValue("2: Object");
+	Thread.sleep(2000);
+	w_t1.selectByValue("3: Object");
+	Thread.sleep(2000);
+	w_t1.selectByValue("4: Object");
+	Thread.sleep(2000);
+	w_t1.selectByValue("5: Object");
+	Thread.sleep(2000);
+	w_t1.selectByValue("6: Object");
+	Thread.sleep(2000);
+	
+	driver.findElement(By.xpath(properties.getProperty("export_excel_icon_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("op_export_excel_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("openrate_cross_click"))).click();
+	Thread.sleep(3000);
+	}
+	else
+	{
+		System.out.println("openrate count is zero");
+	}
+	
+logger.info("Click on Active Recepients");
+
+	WebElement activerec = driver.findElement(By.xpath(properties.getProperty("active_recepients")));
+	if(activerec.isEnabled())
+	{
+	
+	activerec.click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("emails_opened_by_recepients_search_box"))).sendKeys("automated");
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("emails_opened_by_recepients_search"))).click();
+	Thread.sleep(3000);
+	
+	WebDriverWait wait_time_drdn_2 = new WebDriverWait(driver, 50);
+	WebElement w_time2 = wait_time_drdn_2
+			.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(properties.getProperty("time_dropdown"))));  //select time dropdown
+	Thread.sleep(2000);
+	
+	Select w_t2 = new Select(w_time2);
+	w_t2.selectByValue("1: Object");
+	Thread.sleep(2000);
+	w_t2.selectByValue("2: Object");
+	Thread.sleep(2000);
+	w_t2.selectByValue("3: Object");
+	Thread.sleep(2000);
+	w_t2.selectByValue("4: Object");
+	Thread.sleep(2000);
+	w_t2.selectByValue("5: Object");
+	Thread.sleep(2000);
+	w_t2.selectByValue("6: Object");
+	Thread.sleep(2000);
+	
+	driver.findElement(By.xpath(properties.getProperty("export_excel_icon_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("active_export_excel_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("active_cross_click"))).click();
+	Thread.sleep(3000);
+
+	}
+	else
+	{
+		System.out.println("activerecepients count is zero");
+	}
+logger.info("Click on the Clicked URL tile");
+
+	WebElement clickedurl = driver.findElement(By.xpath(properties.getProperty("clicked_URL")));
+	if(clickedurl.isEnabled())
+	{
+
+	clickedurl.click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("recepients_clicked_search_box"))).sendKeys("automated");
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("recepients_clicked_search"))).click();
+	Thread.sleep(3000);
+	
+	WebDriverWait wait_time_drdn_3 = new WebDriverWait(driver, 50);
+	WebElement w_time3 = wait_time_drdn_3
+			.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(properties.getProperty("time_dropdown1"))));  //select time dropdown
+	Thread.sleep(2000);
+	
+	Select w_t3 = new Select(w_time3);
+	w_t3.selectByValue("1: Object");
+	Thread.sleep(2000);
+	w_t3.selectByValue("2: Object");
+	Thread.sleep(2000);
+	w_t3.selectByValue("3: Object");
+	Thread.sleep(2000);
+	w_t3.selectByValue("4: Object");
+	Thread.sleep(2000);
+	
+	driver.findElement(By.xpath(properties.getProperty("export_excel_icon_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("url_export_excel_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("url_cross_click"))).click();
+	Thread.sleep(3000);
+	}
+	
+	else {
+		
+		System.out.println("Clicked URL count is zero");
+	}
+
+	
+logger.info("Click on Hard bounce");
+
+	WebElement hardbounce = driver.findElement(By.xpath(properties.getProperty("click_Hardbounce")));
+	if(hardbounce.isEnabled())
+	{
+		hardbounce.click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("hardbounce_close")));
+		Thread.sleep(3000);
+	}
+	
+	else
+	{
+		System.out.println("Hard bounce value is Zero");
+	}
+	
+	
+	
+logger.info("Click on soft bounce");
+
+	WebElement softbounce = driver.findElement(By.xpath(properties.getProperty("click_softbounce")));
+	if(softbounce.isEnabled())
+	{
+		softbounce.click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("softbounce_close")));
+		Thread.sleep(3000);
+	}
+	
+	else
+	{
+		System.out.println("soft bounce value is Zero");
+	}
+	
+	
+logger.info("Click on unsubscribe");
+
+	WebElement unsubscribe = driver.findElement(By.xpath(properties.getProperty("click_unsubscribe")));
+	if(unsubscribe.isEnabled())
+	{
+		unsubscribe.click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("unsubscribe_close")));
+		Thread.sleep(3000);
+	}
+	
+	else
+	{
+		System.out.println("unsubscribe value is Zero");
+	}
+
+	
+	
+logger.info("Click on Leads");	
+	
+	WebElement leads = driver.findElement(By.xpath(properties.getProperty("click_leads")));
+	
+	
+	if(leads.isEnabled())
+	{
+		leads.click();
+		Thread.sleep(3000);
+		WebDriverWait wait_leadsearch = new WebDriverWait(driver, 50);
+		WebElement waitsearch_lead = wait_leadsearch
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(properties.getProperty("lead_search_click")))); //Search for lead
+		waitsearch_lead.sendKeys("auto_lead");
+		waitsearch_lead.sendKeys(Keys.ENTER);  //Click on search
+		
+		logger.info("Click on filter");
+		driver.findElement(By.xpath(properties.getProperty("click_filter"))).click(); //Click on filter
+		Thread.sleep(3000);
+		
+//		driver.findElement(By.xpath(properties.getProperty("select_from_date"))).click(); // Select From date
+//		Thread.sleep(3000);
+//		
+//		driver.findElement(By.xpath(properties.getProperty("choose_from_date"))).click(); // Select From date
+//		Thread.sleep(3000);
+//		
+//		driver.findElement(By.xpath(properties.getProperty("select_to_date"))).click(); // Select To date
+//		Thread.sleep(3000);
+//		
+//		driver.findElement(By.xpath(properties.getProperty("choose_to_date"))).click(); // Select To date
+//		Thread.sleep(3000);
+		
+		WebDriverWait wait_statusdropdown = new WebDriverWait(driver, 50);
+		WebElement w_statusdropdown = wait_statusdropdown
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(properties.getProperty("status_select_dropdown"))));
+		Thread.sleep(2000);
+		
+	 	Select status_dropdown1 = new Select(w_statusdropdown); 
+	 	status_dropdown1.selectByValue("Opened");
+	 	Thread.sleep(2000);
+	 	
+	 	status_dropdown1.selectByValue("Closed - Lost");
+	 	Thread.sleep(2000);
+	 	
+	 	status_dropdown1.selectByValue("Converted");
+	 	Thread.sleep(2000);
+	 	
+	 	status_dropdown1.selectByValue("Contacted");
+	 	Thread.sleep(2000);
+	 	
+		driver.findElement(By.xpath(properties.getProperty("click_filter_button"))).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("apply_filter_cross"))).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("click_download"))).click();
+		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath(properties.getProperty("leads_close")));
+		Thread.sleep(3000);
+	}
+
+	else
+	{
+		System.out.println("leads count is Zero");
+	}
+
 
 	JavascriptExecutor js1 = (JavascriptExecutor) driver;
 	js1.executeScript("window.scrollTo(0,document.body.scrollHeight)");
