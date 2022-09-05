edce97577838e2577434da66be6a073fda5a97fa
chmounika <chmounika@chmounika-sa>
2022-09-05 19:07:39 +0530

    Committed_Survey_to_Campaign- Campaign based reports(Inprocess)

--- src/main/java/com/xamplify/automation/ManageSurveyToCampaign.java
+++ src/main/java/com/xamplify/automation/ManageSurveyToCampaign.java
@@ -70,19 +70,19 @@ logger.info("Clicked on the Survey tab");
 // 	driver.findElement(By.xpath(properties.getProperty("click_copy_campaign"))).click();
 // 	Thread.sleep(3000);
 // 	driver.findElement(By.xpath(properties.getProperty("click_save_changes"))).click();//click on Save Changes.
-//	Thread.sleep(2000);
+//	Thread.sleep(4000);
 //logger.info("campaign copied successfully");
 //	
-	
-logger.info("click on Gear icon");	
-	driver.findElement(By.xpath(properties.getProperty("tocamp_gearicon1"))).click();  	
-	Thread.sleep(3000);
-	driver.findElement(By.xpath(properties.getProperty("click_Preview"))).click();
-	Thread.sleep(5000);
-	
-	driver.findElement(By.xpath(properties.getProperty("click_close_preview"))).click();
-	Thread.sleep(5000);
-	
+//	
+//logger.info("click on Gear icon");	
+//	driver.findElement(By.xpath(properties.getProperty("tocamp_gearicon1"))).click();  	
+//	Thread.sleep(3000);
+//	driver.findElement(By.xpath(properties.getProperty("click_Preview"))).click();
+//	Thread.sleep(5000);
+//	
+//	driver.findElement(By.xpath(properties.getProperty("click_close_preview"))).click();
+//	Thread.sleep(5000);
+//	
 	
 logger.info("click on Gear icon");
 	driver.findElement(By.xpath(properties.getProperty("tocamp_gearicon1"))).click();  	
@@ -97,8 +97,189 @@ logger.info("click on Gear icon");
 	Thread.sleep(3000);
 	driver.findElement(By.xpath(properties.getProperty("click_active_campaigns"))).click();
 	Thread.sleep(3000);
+logger.info("Click on Campaign based reports");
+	driver.findElement(By.xpath(properties.getProperty("To_Campaign_based_reports"))).click();
+	Thread.sleep(5000);
+logger.info("Click on recepients tile");
+	driver.findElement(By.xpath(properties.getProperty("Survey_To_recepients"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_search_box"))).sendKeys("mounika");
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_recepients_search"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_export_excel_icon_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_export_excel_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_total_recepients_cross_click"))).click();
+	Thread.sleep(3000);
 	
 	
+logger.info("Click on Total Email sent tile");
+	driver.findElement(By.xpath(properties.getProperty("Survey_Total_Email_Sent"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_search_box"))).sendKeys("mounika");
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_recepients_search"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_export_excel_icon_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_export_excel_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_Survey_Total_Email_Sent_cross_click"))).click();
+	Thread.sleep(3000);
+	
+logger.info("Click on Deliverability Tile");
+	driver.findElement(By.xpath(properties.getProperty("Survey_deliverabity"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_search_box"))).sendKeys("mounika");
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_recepients_search"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_export_excel_icon_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_export_excel_click"))).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_Survey_deliverabity_cross_click"))).click();
+	Thread.sleep(3000);
+	
+	
+logger.info("Click on Active Recepients");
+	
+	WebElement survey_to_activerec = driver.findElement(By.xpath(properties.getProperty("survey_to_active_recepients")));
+	if(survey_to_activerec.isEnabled())
+	{
+	
+		survey_to_activerec.click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("survey_to_emails_opened_by_recepients_search_box"))).sendKeys("automated");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("survey_to_emails_opened_by_recepients_search"))).click();
+		Thread.sleep(3000);
+		
+		WebDriverWait wait_time_dropdown1 = new WebDriverWait(driver, 50);
+		WebElement wait_time1 = wait_time_dropdown1
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(properties.getProperty("time_dropdown"))));  //select time dropdown
+		Thread.sleep(2000);
+		
+		Select w_t2 = new Select(wait_time1);
+		w_t2.selectByValue("1: Object");
+		Thread.sleep(2000);
+		w_t2.selectByValue("2: Object");
+		Thread.sleep(2000);
+		w_t2.selectByValue("3: Object");
+		Thread.sleep(2000);
+		w_t2.selectByValue("4: Object");
+		Thread.sleep(2000);
+		w_t2.selectByValue("5: Object");
+		Thread.sleep(2000);
+		w_t2.selectByValue("6: Object");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_export_excel_icon_click"))).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("survey_to_camp_active_recepients_export_excel_click"))).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_Survey_active_recp_cross_click"))).click();
+		Thread.sleep(3000);
+
+	}
+	else
+	{
+		System.out.println("activerecepients count is zero");
+	}
+	
+		
+
+logger.info("Click on openrate");
+
+	WebElement survey_to_openrate = driver.findElement(By.xpath(properties.getProperty("click_survey_to_camp_openrate")));
+	if(survey_to_openrate.isEnabled())
+	{
+	
+		survey_to_openrate.click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("survey_to_emails_opened_by_recepients_search_box"))).sendKeys("automated");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("survey_to_emails_opened_by_recepients_search"))).click();
+		Thread.sleep(3000);
+		
+		WebDriverWait wait_time_dropdown2 = new WebDriverWait(driver, 50);
+		WebElement wait_time2 = wait_time_dropdown2
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(properties.getProperty("time_dropdown"))));  //select time dropdown
+		Thread.sleep(2000);
+		
+		Select w_t2 = new Select(wait_time2);
+		w_t2.selectByValue("1: Object");
+		Thread.sleep(2000);
+		w_t2.selectByValue("2: Object");
+		Thread.sleep(2000);
+		w_t2.selectByValue("3: Object");
+		Thread.sleep(2000);
+		w_t2.selectByValue("4: Object");
+		Thread.sleep(2000);
+		w_t2.selectByValue("5: Object");
+		Thread.sleep(2000);
+		w_t2.selectByValue("6: Object");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_export_excel_icon_click"))).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("survey_to_camp_openrate_export_excel_click"))).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_openrate_cross_click"))).click();
+		Thread.sleep(3000);
+	
+	}
+	else
+	{
+		System.out.println("activerecepients count is zero");
+	}
+	
+		
+
+	
+	logger.info("Click on the Clicked URL tile");
+	
+	WebElement clickedurl = driver.findElement(By.xpath(properties.getProperty("survey_to_clicked_URL")));
+	if(clickedurl.isEnabled())
+	{
+	
+		clickedurl.click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("Torecepients_clicked_search_box"))).sendKeys("automated");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("Torecepients_clicked_search"))).click();
+		Thread.sleep(3000);
+		
+		WebDriverWait wait_time_drdn_3 = new WebDriverWait(driver, 50);
+		WebElement w_time3 = wait_time_drdn_3
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(properties.getProperty("time_dropdown1"))));  //select time dropdown
+		Thread.sleep(2000);
+		
+		Select w_t3 = new Select(w_time3);
+		w_t3.selectByValue("1: Object");
+		Thread.sleep(2000);
+		w_t3.selectByValue("2: Object");
+		Thread.sleep(2000);
+		w_t3.selectByValue("3: Object");
+		Thread.sleep(2000);
+		w_t3.selectByValue("4: Object");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_export_excel_icon_click"))).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("survey_to_camp_clicked_URL_export_excel_click"))).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("survey_to_camp_analytics_clicked_URL_cross_click"))).click();
+		Thread.sleep(3000);
 		}
+		
+		else {
+			
+			System.out.println("Clicked URL count is zero");
+		}
+
+}
 
 }
