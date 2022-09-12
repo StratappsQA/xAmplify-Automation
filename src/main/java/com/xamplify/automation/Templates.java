2633010155dd57f191a1202bd641aca7e44275c2
mharish <mharish@mharish-sa>
2022-09-12 22:39:28 +0530

    Templates

--- src/main/java/com/xamplify/automation/Templates.java
+++ src/main/java/com/xamplify/automation/Templates.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -22,7 +23,7 @@ public class Templates {
 	@BeforeClass
 	public void setup() {
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mharish\\Desktop\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mharish\\Desktop\\New folder (2)\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://xamplify.co/login");
@@ -36,19 +37,20 @@ public class Templates {
 		driver.findElement(By.cssSelector("button[type*='submit']")).click();
 	}
 
-	@Test(priority = 2, enabled = false)
+	@Test(priority = 2, enabled = true)
 	public void design() throws InterruptedException {
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("/html/body/app-root/app-home/div/app-leftsidebar/div/div/ul/li[5]/a/span[1]"))
 				.click();
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("(//span[@class='design-name mDisableColor'])[1]")).click();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("(//li[@class='filter'])[1]")).click();
+		
 	}
 
 	@Test(priority = 3, enabled = false)
 	public void design1() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("(//li[@class='filter'])[1]")).click();
 
 		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).click();
 		Thread.sleep(2000);
@@ -321,7 +323,7 @@ public class Templates {
 		driver.findElement(By.xpath("//*[@id='save']")).click();
 	}
 
-	@Test(priority = 14, enabled = true)
+	@Test(priority = 14, enabled = false)
 	public void managetemplates() throws InterruptedException {
 
 		Thread.sleep(3000);
@@ -355,14 +357,231 @@ public class Templates {
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//*[@id='manage-email-template-list']/tbody/tr[1]/td[5]/div/div[2]/a[2]/i"))
 				.click();
-		driver.findElement(By.xpath("//*[@id='show_email_template_preivew']/div/div/div[1]/button")).click();
-	}
-		@Test(priority = 15, enabled = true)
-		public void managetemplates1() throws InterruptedException {
-
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//i[@class='fa fa-times'])[2]")).click();
+		Thread.sleep(2000);
+		Select dropdown = new Select(driver. findElement(By.xpath("/html/body/app-root/app-home/div/div/app-manage-template/div/div[3]/div/div/div[1]/div/div[2]/div/div/div[1]/select")));
+		dropdown. selectByVisibleText("Name (A-Z)");
 		
+	}
+	
+	
+	
+	@Test(priority = 14, enabled = false)
+	public void managetemplates_1() throws InterruptedException {
+
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("/html/body/app-root/app-home/div/app-leftsidebar/div/div/ul/li[5]/a/span[1]"))
+				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@class='design-name']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//ul/li[@class='filter'][2]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).sendKeys("video1");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a/i[@class='fa fa-inbox']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id='email_spam_check']/div/div/div[3]/a")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a/i[@class='fa fa-pencil-square-o mDisableColor']")).click();
+		Thread.sleep(15000);
+		driver.switchTo().frame(0);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[contains(text(),'SAVE')]/..")).click();
+		Thread.sleep(2000);
+
+		driver.switchTo().defaultContent();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div/input[@value='Update']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id='manage-email-template-list']/tbody/tr[1]/td[5]/div/div[2]/a[2]/i"))
+				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//i[@class='fa fa-times'])[2]")).click();
 		
+	
+	
+	
+	
+	
+		/*@Test(priority = 15, enabled = true)
+		public void uplod_custom() throws InterruptedException {
+			Thread.sleep(2000);
+			
+			driver.findElement(By.xpath("(//li[@class='filter'])[5]")).click();
+			Thread.sleep(3000);
+
+			WebElement ele = driver.findElement(By.xpath("/html/body/app-root/app-home/div/div/app-select-template/div[1]/div[2]/div/div/div[2]/div/div[2]/div/div"));
+
+			// Creating object of an Actions class
+			Actions action = new Actions(driver);
+
+			// Performing the mouse hover action on the target element.
+			action.moveToElement(ele).perform();
+			driver.findElement(By.xpath("(//a/i[@class='fa fa-plus-circle'])[1]"))
+					.click();
+Thread.sleep(2000);
+driver.findElement(By.xpath("//div/input[@id='name']")).sendKeys("harish");
+
+
+*/
+
 
 	}
+	
+	
+	@Test(priority = 15, enabled = false)
+	public void managetemplates_2() throws InterruptedException {
 
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("/html/body/app-root/app-home/div/app-leftsidebar/div/div/ul/li[5]/a/span[1]"))
+				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@class='design-name']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//ul/li[@class='filter'][4]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).sendKeys("email");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a/i[@class='fa fa-inbox']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id='email_spam_check']/div/div/div[3]/a")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a/i[@class='fa fa-pencil-square-o mDisableColor']")).click();
+		Thread.sleep(20000);
+		driver.switchTo().frame(0);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[contains(text(),'SAVE')]/..")).click();
+		Thread.sleep(2000);
+
+		driver.switchTo().defaultContent();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div/input[@value='Update']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id='manage-email-template-list']/tbody/tr[1]/td[5]/div/div[2]/a[2]/i"))
+				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//i[@class='fa fa-times'])[2]")).click();
+	}
+	
+	@Test(priority = 15, enabled = false)
+	public void managetemplates_3() throws InterruptedException {
+
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("/html/body/app-root/app-home/div/app-leftsidebar/div/div/ul/li[5]/a/span[1]"))
+				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@class='design-name']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//ul/li[@class='filter'][5]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).sendKeys("vid");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a/i[@class='fa fa-inbox']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id='email_spam_check']/div/div/div[3]/a")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a/i[@class='fa fa-pencil-square-o mDisableColor']")).click();
+		Thread.sleep(20000);
+		driver.switchTo().frame(0);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[contains(text(),'SAVE')]/..")).click();
+		Thread.sleep(2000);
+
+		driver.switchTo().defaultContent();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div/input[@value='Update']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id='manage-email-template-list']/tbody/tr[1]/td[5]/div/div[2]/a[2]/i"))
+				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//i[@class='fa fa-times'])[2]")).click();
+	}
+	@Test(priority = 16, enabled = false)
+	public void managetemplates_5() throws InterruptedException {
+
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("/html/body/app-root/app-home/div/app-leftsidebar/div/div/ul/li[5]/a/span[1]"))
+				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@class='design-name']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//ul/li[@class='filter'][6]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).sendKeys("event");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a/i[@class='fa fa-inbox']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id='email_spam_check']/div/div/div[3]/a")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a/i[@class='fa fa-pencil-square-o mDisableColor']")).click();
+		Thread.sleep(20000);
+		driver.switchTo().frame(0);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[contains(text(),'SAVE')]/..")).click();
+		Thread.sleep(2000);
+
+		driver.switchTo().defaultContent();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div/input[@value='Update']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id='manage-email-template-list']/tbody/tr[1]/td[5]/div/div[2]/a[2]/i"))
+				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//i[@class='fa fa-times'])[2]")).click();
+	}
+	@Test(priority = 17, enabled = true)
+	public void managetemplates_4() throws InterruptedException {
+
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("/html/body/app-root/app-home/div/app-leftsidebar/div/div/ul/li[5]/a/span[1]"))
+				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@class='design-name']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//ul/li[@class='filter'][7]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).sendKeys("eventco");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Search for a template']")).sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a/i[@class='fa fa-inbox']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id='email_spam_check']/div/div/div[3]/a")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a/i[@class='fa fa-pencil-square-o mDisableColor']")).click();
+		Thread.sleep(20000);
+		driver.switchTo().frame(0);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[contains(text(),'SAVE')]/..")).click();
+		Thread.sleep(2000);
+
+		driver.switchTo().defaultContent();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div/input[@value='Update']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id='manage-email-template-list']/tbody/tr[1]/td[5]/div/div[2]/a[2]/i"))
+				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//i[@class='fa fa-times'])[2]")).click();
+	}
 }
