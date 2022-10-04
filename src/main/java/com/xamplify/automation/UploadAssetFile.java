ab28deffdd52682b794f534b4ad40fe13b4691e7
spriyanka <spriyanka@spriyanka-sa>
2022-10-04 19:22:33 +0530

    Added all types for files to upload asset

--- src/main/java/com/xamplify/automation/UploadAssetFile.java
+++ src/main/java/com/xamplify/automation/UploadAssetFile.java
@@ -26,27 +26,28 @@ public class UploadAssetFile {
 	final Logger logger = LogManager.getLogger(UploadAssetFile.class);
 	
 
-	    @Test 
+	    @Test (priority = 0)
 	    public void uploadasset_png() throws InterruptedException, IOException {	
 		Thread.sleep(3000);
-		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click();
+		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
 		Thread.sleep(5000);
-		driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click();
+		driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click(); //click on design/upload
 		Thread.sleep(3000);
-		driver.findElement(By.xpath(properties.getProperty("Upload"))).click();
+		driver.findElement(By.xpath(properties.getProperty("Upload"))).click(); // click on upload
 		Thread.sleep(5000);
-		driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click();
+		driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click(); //click on Browse
 		Thread.sleep(5000);
-		Runtime.getRuntime().exec("D:\\Selenium\\asset_png.exe");
+		Runtime.getRuntime().exec("D:\\Selenium\\asset_png.exe");  // upload file 
 		logger.info("asset png uploaded");
 		Thread.sleep(5000);
-		driver.findElement(By.xpath(properties.getProperty("Thumbnail"))).click();
+		driver.findElement(By.xpath(properties.getProperty("Thumbnail"))).click(); //click on thumbnail icon
+		Thread.sleep(5000);
+		Runtime.getRuntime().exec("D:\\Selenium\\Thumbnail.exe"); //upload thumbnail image
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("Savechanges"))).click(); 
+		Thread.sleep(5000);
 		logger.info("thumbnail uploaded");
-		Thread.sleep(5000);
-		Runtime.getRuntime().exec("D:\\Selenium\\Thumbnail.exe");
-		Thread.sleep(5000);
-		driver.findElement(By.xpath(properties.getProperty("Savechanges"))).click();
-		Thread.sleep(5000);
+
 		driver.findElement(By.xpath(properties.getProperty("Name"))).sendKeys(properties.getProperty("png_name"));
 		Thread.sleep(5000);
 		driver.switchTo().frame(0);
@@ -61,23 +62,23 @@ public class UploadAssetFile {
 		String actualresult = success.getText();
 		String expectedresult = "Uploaded Successfully";
 		Assert.assertEquals(actualresult, expectedresult);
-		logger.info("Assertion for uploadsucess message");
+		logger.info("Assertion succesfull for success message ");
 		Thread.sleep(5000);
 		driver.findElement(By.xpath(properties.getProperty("Refresh"))).click();
 	    }
 		
 		
 		
-		 @Test 
+		 @Test (priority = 1)
 		    public void uploadasset_mp3() throws InterruptedException, IOException {	
 			Thread.sleep(3000);
-			driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click();
+			driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
 			Thread.sleep(5000);
-			driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click();
+			driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click(); //click on design/upload
 			Thread.sleep(3000);
-			driver.findElement(By.xpath(properties.getProperty("Upload"))).click();
+			driver.findElement(By.xpath(properties.getProperty("Upload"))).click(); // click on upload
 			Thread.sleep(5000);
-			driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click();
+			driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click(); //click on Browse
 			Thread.sleep(5000);
 			Runtime.getRuntime().exec("D:\\Selenium\\asset_mp3.exe");
 			logger.info("asset mp3 file uploaded");
@@ -98,7 +99,7 @@ public class UploadAssetFile {
 			driver.switchTo().defaultContent();
 			driver.findElement(By.xpath(properties.getProperty("Submit"))).click();
 			Thread.sleep(5000);
-			logger.info("Asset uploaded succesfully");
+			logger.info("Asset mp3 uploaded succesfully");
 			WebElement success = driver.findElement(By.xpath(properties.getProperty("Success")));	
 			String actualresult = success.getText();
 			String expectedresult = "Uploaded Successfully";
@@ -109,19 +110,19 @@ public class UploadAssetFile {
 		 }
 		
 		
-			 @Test 
+			 @Test (priority = 2)
 			    public void uploadasset_ppt() throws InterruptedException, IOException {	
 				Thread.sleep(3000);
-				driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click();
+				driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
 				Thread.sleep(5000);
-				driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click();
+				driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click(); //click on design/upload
 				Thread.sleep(3000);
-				driver.findElement(By.xpath(properties.getProperty("Upload"))).click();
+				driver.findElement(By.xpath(properties.getProperty("Upload"))).click(); // click on upload
 				Thread.sleep(5000);
-				driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click();
+				driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click(); //click on Browse
 				Thread.sleep(5000);
 				Runtime.getRuntime().exec("D:\\Selenium\\asset_ppt.exe");
-				logger.info("asset mp3 file uploaded");
+				logger.info("asset ppt file uploaded");
 				Thread.sleep(5000);
 				driver.findElement(By.xpath(properties.getProperty("Thumbnail"))).click();
 				logger.info("thumbnail uploaded");
@@ -150,19 +151,19 @@ public class UploadAssetFile {
 			 }
 		
 		
-				 @Test 
+				 @Test (priority = 3)
 				    public void uploadasset_doc() throws InterruptedException, IOException {	
 					Thread.sleep(3000);
-					driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click();
+					driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
 					Thread.sleep(5000);
-					driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click();
+					driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click(); //click on design/upload
 					Thread.sleep(3000);
-					driver.findElement(By.xpath(properties.getProperty("Upload"))).click();
+					driver.findElement(By.xpath(properties.getProperty("Upload"))).click(); // click on upload
 					Thread.sleep(5000);
-					driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click();
+					driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click(); //click on Browse
 					Thread.sleep(5000);
 					Runtime.getRuntime().exec("D:\\Selenium\\asset_doc.exe");
-					logger.info("asset mp3 file uploaded");
+					logger.info("asset doc file uploaded");
 					Thread.sleep(5000);
 					driver.findElement(By.xpath(properties.getProperty("Thumbnail"))).click();
 					logger.info("thumbnail uploaded");
@@ -191,19 +192,19 @@ public class UploadAssetFile {
 				 }
 		
 		
-					 @Test 
+					 @Test (priority = 4)
 					    public void uploadasset_xls() throws InterruptedException, IOException {	
 						Thread.sleep(3000);
-						driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click();
+						driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
 						Thread.sleep(5000);
-						driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click();
+						driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click(); //click on design/upload
 						Thread.sleep(3000);
-						driver.findElement(By.xpath(properties.getProperty("Upload"))).click();
+						driver.findElement(By.xpath(properties.getProperty("Upload"))).click(); // click on upload
 						Thread.sleep(5000);
-						driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click();
+						driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click(); //click on Browse
 						Thread.sleep(5000);
 						Runtime.getRuntime().exec("D:\\Selenium\\asset_xls.exe");
-						logger.info("asset mp3 file uploaded");
+						logger.info("asset xls file uploaded");
 						Thread.sleep(5000);
 						driver.findElement(By.xpath(properties.getProperty("Thumbnail"))).click();
 						logger.info("thumbnail uploaded");
@@ -233,19 +234,19 @@ public class UploadAssetFile {
 		
 		
 		
-					 @Test 
+					 @Test (priority = 5)
 					    public void uploadasset_gif() throws InterruptedException, IOException {	
 						Thread.sleep(3000);
-						driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click();
+						driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
 						Thread.sleep(5000);
-						driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click();
+						driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click(); //click on design/upload
 						Thread.sleep(3000);
-						driver.findElement(By.xpath(properties.getProperty("Upload"))).click();
+						driver.findElement(By.xpath(properties.getProperty("Upload"))).click(); // click on upload
 						Thread.sleep(5000);
-						driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click();
+						driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click(); //click on Browse
 						Thread.sleep(5000);
 						Runtime.getRuntime().exec("D:\\Selenium\\asset_gif.exe");
-						logger.info("asset mp3 file uploaded");
+						logger.info("asset gif file uploaded");
 						Thread.sleep(5000);
 						driver.findElement(By.xpath(properties.getProperty("Thumbnail"))).click();
 						logger.info("thumbnail uploaded");
@@ -274,19 +275,20 @@ public class UploadAssetFile {
 					 }
 		
 		
-					 @Test 
+					 @Test (priority = 6)
 					    public void uploadasset_jpg() throws InterruptedException, IOException {	
 						Thread.sleep(3000);
-						driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click();
+						driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
 						Thread.sleep(5000);
-						driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click();
+						driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click(); //click on design/upload
 						Thread.sleep(3000);
-						driver.findElement(By.xpath(properties.getProperty("Upload"))).click();
+						driver.findElement(By.xpath(properties.getProperty("Upload"))).click(); // click on upload
 						Thread.sleep(5000);
-						driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click();
+						driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click(); //click on Browse
+						
 						Thread.sleep(5000);
 						Runtime.getRuntime().exec("D:\\Selenium\\asset_jpg.exe");
-						logger.info("asset mp3 file uploaded");
+						logger.info("asset jpg file uploaded");
 						Thread.sleep(5000);
 						driver.findElement(By.xpath(properties.getProperty("Thumbnail"))).click();
 						logger.info("thumbnail uploaded");
@@ -315,19 +317,19 @@ public class UploadAssetFile {
 					 }
 		
 		
-					 @Test 
+					 @Test (priority = 7)
 					    public void uploadasset_zip() throws InterruptedException, IOException {	
 						Thread.sleep(3000);
-						driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click();
+						driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
 						Thread.sleep(5000);
-						driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click();
+						driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click(); //click on design/upload
 						Thread.sleep(3000);
-						driver.findElement(By.xpath(properties.getProperty("Upload"))).click();
+						driver.findElement(By.xpath(properties.getProperty("Upload"))).click(); // click on upload
 						Thread.sleep(5000);
-						driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click();
+						driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click(); //click on Browse
 						Thread.sleep(5000);
 						Runtime.getRuntime().exec("D:\\Selenium\\asset_zip.exe");
-						logger.info("asset mp3 file uploaded");
+						logger.info("asset zip file uploaded");
 						Thread.sleep(5000);
 						driver.findElement(By.xpath(properties.getProperty("Thumbnail"))).click();
 						logger.info("thumbnail uploaded");
@@ -355,19 +357,19 @@ public class UploadAssetFile {
 						driver.findElement(By.xpath(properties.getProperty("Refresh"))).click();
 					 }
 		
-					 @Test 
+					 @Test (priority = 8)
 					    public void uploadasset_html() throws InterruptedException, IOException {	
 						Thread.sleep(3000);
-						driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click();
+						driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
 						Thread.sleep(5000);
-						driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click();
+						driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click(); //click on design/upload
 						Thread.sleep(3000);
-						driver.findElement(By.xpath(properties.getProperty("Upload"))).click();
+						driver.findElement(By.xpath(properties.getProperty("Upload"))).click(); // click on upload
 						Thread.sleep(5000);
-						driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click();
+						driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click(); //click on Browse
 						Thread.sleep(5000);
 						Runtime.getRuntime().exec("D:\\Selenium\\asset_html.exe");
-						logger.info("asset mp3 file uploaded");
+						logger.info("asset html file uploaded");
 						Thread.sleep(5000);
 						driver.findElement(By.xpath(properties.getProperty("Thumbnail"))).click();//upload thumbnail
 						logger.info("thumbnail uploaded");
@@ -396,19 +398,19 @@ public class UploadAssetFile {
 					 }
 		
 		
-					 @Test 
+					 @Test (priority = 9)
 					    public void uploadasset_svg() throws InterruptedException, IOException {	
 						Thread.sleep(3000);
-						driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on content left menu
+						driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
 						Thread.sleep(5000);
-						driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click();
+						driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click(); //click on design/upload
 						Thread.sleep(3000);
-						driver.findElement(By.xpath(properties.getProperty("Upload"))).click();
+						driver.findElement(By.xpath(properties.getProperty("Upload"))).click(); // click on upload
 						Thread.sleep(5000);
-						driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click();
+						driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click(); //click on Browse
 						Thread.sleep(5000);
 						Runtime.getRuntime().exec("D:\\Selenium\\asset_svg.exe");
-						logger.info("asset mp3 file uploaded");
+						logger.info("asset svg file uploaded");
 						Thread.sleep(5000);
 						driver.findElement(By.xpath(properties.getProperty("Thumbnail"))).click();
 						logger.info("thumbnail uploaded");
