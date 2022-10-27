4be65079397ed48f1b6b520c84642894ab199289
spriyanka <spriyanka@spriyanka-sa>
2022-10-27 18:23:10 +0530

    commit-partner account-access shared assets-inprogress

---
+++ src/main/java/com/xamplify/automation/AccessSharedAsset.java
@@ -0,0 +1,57 @@
+package com.xamplify.automation;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class AccessSharedAsset {
+	
+	
+	WebDriver driver = Instance.getInstance();
+	Properties properties = PropertiesFile
+			.readPropertyFile("D:\\git\\xAmplify-Automation\\src\\main\\resources\\AssetLibrary.properties");
+	final Logger logger = LogManager.getLogger(AccessSharedAsset.class);
+
+	
+	@Test (priority = 1,enabled = true)
+	public void sharedasset() throws InterruptedException, IOException {	
+		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("access_sharedasset"))).click(); //click on access shared asset
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("Searchbar"))).sendKeys("partnerasset"); //search with partnerasset 
+		Thread.sleep(5000);	
+		driver.findElement(By.xpath(properties.getProperty("Searchclick"))).click(); //click on search icon
+		Thread.sleep(5000);	
+		driver.findElement(By.xpath(properties.getProperty("partner_asset_view"))).click();  //click on view icon of shared asset
+		logger.info("Assetfile has been viewed in partner account successfully");
+		Thread.sleep(5000);	
+		driver.findElement(By.xpath(properties.getProperty("partner_asset_download"))).click();  //click on download button of shared asset
+		logger.info("Assetfile has been downloaded in partner account successfully");
+		Thread.sleep(5000);	
+		driver.findElement(By.xpath(properties.getProperty("partner_cross_icon"))).click(); //click on cross icon
+		Thread.sleep(5000);	
+		driver.findElement(By.xpath(properties.getProperty("Searchbar"))).sendKeys("partnerasset"); //search with partnerasset 
+		Thread.sleep(5000);	
+		driver.findElement(By.xpath(properties.getProperty("Searchclick"))).click(); //click on search icon
+		Thread.sleep(5000);	
+		driver.findElement(By.xpath(properties.getProperty("partner_view_analytics"))).click(); //click on analytics icon
+		Thread.sleep(10000);	
+		logger.info("Analytics have been viewed succesfully in partner account for the assetfile");	
+		driver.findElement(By.xpath(properties.getProperty("partner_only_view"))).click(); //click on view button in analytics
+		Thread.sleep(5000);	
+		logger.info("displayed only viewed assets");	
+		driver.findElement(By.xpath(properties.getProperty("partner_only_download"))).click(); //click on download button in analytics
+		Thread.sleep(5000);	
+		logger.info("displayed only downloaded assets");
+		
+		
+		
+	}
+}
+
--- src/main/java/com/xamplify/automation/UploadAssetFile.java
+++ src/main/java/com/xamplify/automation/UploadAssetFile.java
@@ -27,7 +27,7 @@ public class UploadAssetFile {
 
 
 	//uploading png file
-	@Test (priority = 0)
+	@Test (priority = 0,enabled = false)
 	public void uploadasset_png() throws InterruptedException, IOException {	
 		Thread.sleep(3000);
 		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
@@ -84,7 +84,7 @@ public class UploadAssetFile {
 
 
 	//uploading mp3 file
-	@Test (priority = 1)
+	@Test (priority = 1,enabled = false)
 	public void uploadasset_mp3() throws InterruptedException, IOException {	
 		Thread.sleep(3000);
 		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
@@ -132,7 +132,7 @@ public class UploadAssetFile {
 
 
 	//uploading ppt file
-	@Test (priority = 2)
+	@Test (priority = 2,enabled = false)
 	public void uploadasset_ppt() throws InterruptedException, IOException {	
 		Thread.sleep(3000);
 		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
@@ -180,7 +180,7 @@ public class UploadAssetFile {
 
 
 	//uploading doc file
-	@Test (priority = 3)
+	@Test (priority = 3,enabled = false)
 	public void uploadasset_doc() throws InterruptedException, IOException {	
 		Thread.sleep(3000);
 		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
@@ -228,7 +228,7 @@ public class UploadAssetFile {
 
 
 	//uploading xls file
-	@Test (priority = 4)
+	@Test (priority = 4,enabled = false)
 	public void uploadasset_xls() throws InterruptedException, IOException {	
 		Thread.sleep(3000);
 		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
@@ -278,7 +278,7 @@ public class UploadAssetFile {
 
 
 	//uploading gif file
-	@Test (priority = 5)
+	@Test (priority = 5,enabled = false)
 	public void uploadasset_gif() throws InterruptedException, IOException {	
 		Thread.sleep(3000);
 		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
@@ -325,7 +325,7 @@ public class UploadAssetFile {
 	}
 
 	//uploading jpg file
-	@Test (priority = 6)
+	@Test (priority = 6,enabled = false)
 	public void uploadasset_jpg() throws InterruptedException, IOException {	
 		Thread.sleep(3000);
 		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
@@ -373,7 +373,7 @@ public class UploadAssetFile {
 	}
 
 	//uploading zip file
-	@Test (priority = 7)
+	@Test (priority = 7,enabled = false)
 	public void uploadasset_zip() throws InterruptedException, IOException {	
 		Thread.sleep(3000);
 		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
@@ -420,7 +420,7 @@ public class UploadAssetFile {
 	}
 
 	//uploading html file
-	@Test (priority = 8)
+	@Test (priority = 8,enabled = false)
 	public void uploadasset_html() throws InterruptedException, IOException {	
 		Thread.sleep(3000);
 		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
@@ -468,7 +468,7 @@ public class UploadAssetFile {
 
 
 	//uploading svg file
-	@Test (priority = 9)
+	@Test (priority = 9,enabled = false)
 	public void uploadasset_svg() throws InterruptedException, IOException {	
 		Thread.sleep(3000);
 		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
@@ -512,8 +512,93 @@ public class UploadAssetFile {
 		logger.info("Assertion for uploadsucess message of uploading svg file");
 		Thread.sleep(5000);
 		driver.findElement(By.xpath(properties.getProperty("Refresh"))).click();
-
 	}
+	
+	
+	//uploading asset to view&downlod in partner account
+	@Test (priority = 1,enabled = true)
+	public void assetpng() throws InterruptedException, IOException {	
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("Content_leftmenu"))).click(); //click on left side content menu
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("DesignUpload"))).click(); //click on design/upload
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("Upload"))).click(); // click on upload
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("BrowseButton"))).click(); //click on Browse
+		Thread.sleep(5000);
+		Runtime.getRuntime().exec("D:\\Selenium\\asset_png.exe");  // upload png file 
+		logger.info("asset png uploaded");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("Thumbnail"))).click(); //click on thumbnail icon
+		Thread.sleep(5000);
+		Runtime.getRuntime().exec("D:\\Selenium\\Thumbnail.exe"); //upload thumbnail image
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("Savechanges"))).click();  //click on save changes
+		Thread.sleep(5000);
+		logger.info("thumbnail uploaded");
+		driver.findElement(By.xpath(properties.getProperty("Name"))).sendKeys(properties.getProperty("assetname")+ "_" + System.currentTimeMillis());
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("folder_dropdown_click1"))).click(); //click on folder dropdown
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("folder_selection1"))).click(); //select folder
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("tag_plusicon"))).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("tag_text_click"))).click();
+		//	driver.findElement(By.xpath(properties.getProperty("tag_text_click"))).sendKeys(properties.getProperty("tag_name"));
+		WebElement enter=driver.findElement(By.xpath(properties.getProperty("tag_text_click")));
+		enter.sendKeys(properties.getProperty("tag_name")+ "_" + System.currentTimeMillis());
+		enter.sendKeys(Keys.ENTER);
+		driver.findElement(By.xpath(properties.getProperty("tag_savebutton"))).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("tag_select"))).click();
+		Thread.sleep(5000);
+		driver.switchTo().frame(0);
+		driver.findElement(By.xpath(properties.getProperty("Descriptionclick"))).click();
+		driver.findElement(By.xpath(properties.getProperty("Descriptionclick"))).sendKeys("Asset_Description"); //enter description
+		Thread.sleep(5000);
+		driver.switchTo().defaultContent();
+		driver.findElement(By.xpath(properties.getProperty("Submit"))).click(); //click on submit 
+		Thread.sleep(5000);
+		logger.info("Asset png uploaded succesfully");
+		WebElement success = driver.findElement(By.xpath(properties.getProperty("Success")));	
+		String actualresult = success.getText();
+		String expectedresult = "Uploaded Successfully";
+		Assert.assertEquals(actualresult, expectedresult);
+		logger.info("Assertion succesfull for success message of uploading png file ");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("Refresh"))).click(); //click on refresh icon file has been saved		
+		//search with the created partnerasset and publish to partner
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("Searchbar"))).sendKeys("partnerasset");
+		Thread.sleep(5000);	
+		driver.findElement(By.xpath(properties.getProperty("Searchclick"))).click(); 
+		Thread.sleep(5000);	
+		driver.findElement(By.xpath(properties.getProperty("publish_click"))).click(); //click on publish link
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("Searchbar_publish"))).sendKeys("automatedPartner");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("Searchclick_publish"))).click();		
+		Thread.sleep(5000);		
+		driver.findElement(By.xpath(properties.getProperty("arrow_click"))).click(); //select arrow of partner company
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("partner_select"))).click(); //select partner
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("submit_button"))).click();
+		Thread.sleep(5000);
+		logger.info("Assetfile published succesfully");
+		//Assertion 1st way for published asset
+		Thread.sleep(5000);
+		WebElement published = driver.findElement(By.xpath(properties.getProperty("published_page")));	
+		String actualresult2 = published.getText();
+		String expectedresult2 = "Published Successfully";				
+		Assert.assertEquals(actualresult2, expectedresult2);
+		logger.info("Assertion for published succesfull");	
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(properties.getProperty("cross_icon_publish"))).click();
+		logger.info("cross icon clicked");		
+	}	
 }
 
 
