f649d9a384a4b013008ea175392de461774f00f1
agayatri <agayatri@agayatri1-sa>
2022-11-18 19:06:18 +0530

    Commit-partner acc-working on Redistribution Event Campaign

---
+++ src/main/java/com/xamplify/automation/RedistributeEventCampaign.java
@@ -0,0 +1,170 @@
+package com.xamplify.automation;
+
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class RedistributeEventCampaign extends ExtentReport {
+	WebDriver driver = Instance.getInstance();
+	Properties properties = PropertiesFile.readPropertyFile(
+			"D:\\Workspace2\\xAmplify-Automation\\src\\main\\resources\\RedistributionCampaign.properties");// properties
+																											// file
+	final Logger logger = LogManager.getLogger(RedistributeEventCampaign.class);
+
+	@Test
+	public void hoverRedistributecampaigns_event() throws InterruptedException {
+		Thread.sleep(7000);
+
+		WebElement eve_ele = driver.findElement(By.linkText("Campaign"));
+		Actions act = new Actions(driver);
+		act.moveToElement(eve_ele).perform();
+		driver.findElement(By.xpath(properties.getProperty("partner_eve_redistribute_evehover"))).click(); // hover on
+																											// campaign
+		Thread.sleep(6000);
+
+		driver.findElement(By.xpath(properties.getProperty("partner_eve_redistribute_eventtab"))).click(); // click on
+																											// event
+																											// tab
+		Thread.sleep(38000);
+		logger.info("Event tab clicked successfully");
+
+		
+		driver.findElement(By.xpath(properties.getProperty("partner_redistribute_eve_camp_preview"))).click(); // click on
+		// preview
+		Thread.sleep(6000);
+
+		logger.info("Event tab preview clicked successfully");
+
+		driver.findElement(By.xpath(properties.getProperty("partner_redistribute_eve_camp_preview_close"))).click(); // click on
+		
+		
+		
+		
+		
+		logger.info("Event tab preview closed successfully");
+
+		
+		driver.findElement(By.xpath(properties.getProperty("partner_eve_redis_eve_download"))).click(); // click on
+																										// download
+		// icon
+
+		logger.info("Event download clicked successfully");
+
+		Thread.sleep(4000);
+		driver.findElement(By.xpath(properties.getProperty("partner_eve_redis_eve_dwnld_html"))).click(); // click on
+		// download html
+// icon
+		logger.info("Event download html clicked successfully");
+Thread.sleep(4000);
+driver.findElement(By.xpath(properties.getProperty("partner_eve_redis_viewdwnld_history"))).click(); // click on history
+
+Thread.sleep(4000);
+logger.info("Event view download history clicked successfully");
+driver.findElement(By.xpath(properties.getProperty("partner_eve_redis_viewdwnld_history_close"))).click(); // click on history
+
+
+Thread.sleep(4000);
+
+driver.get("https://xamplify.co/home/campaigns/manage");
+
+Thread.sleep(7000);
+
+logger.info("Redirected to ManageCampaign successfully");
+driver.findElement(By.xpath(properties.getProperty("partner_manage_eve_gearicon"))).click();
+
+Thread.sleep(4000);
+
+
+driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_edit"))).click();
+Thread.sleep(4000);
+logger.info("Clicked on the edit icon successfully");
+driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_edit_folder"))).click();
+Thread.sleep(4000);
+logger.info("Clicked on the edit folder successfully");
+
+driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_edit_updatefolder"))).click();
+Thread.sleep(4000);
+
+driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_edit_update"))).click();
+Thread.sleep(4000);
+logger.info("Clicked on the update button successfully");
+
+driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_edit_update_close"))).click();
+Thread.sleep(9000);
+
+
+
+driver.findElement(By.xpath(properties.getProperty("partner_manage_eve_gearicon"))).click();
+
+Thread.sleep(4000);
+
+
+
+
+
+driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_sendpreviewemail"))).click();
+Thread.sleep(4000);
+logger.info("Clicked on the send preview email option successfully");
+
+
+
+
+WebElement sndata=driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_senddata")));
+sndata.sendKeys("agayatri@stratapps.com");
+sndata.sendKeys(Keys.ENTER);
+
+Thread.sleep(7000);
+
+Actions pa = new Actions(driver);
+pa.sendKeys(Keys.PAGE_DOWN).build().perform(); // scroll down the page
+
+Thread.sleep(4000);
+
+
+
+driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_sendbutton"))).click();
+Thread.sleep(4000);
+
+driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_closebutton"))).click();
+Thread.sleep(9000);
+
+logger.info("Clicked on the close option successfully");
+
+
+driver.findElement(By.xpath(properties.getProperty("partner_manage_eve_gearicon"))).click();
+
+Thread.sleep(4000);
+
+
+driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_invitemore"))).click();
+Thread.sleep(4000);
+
+logger.info("Clicked on the InviteMore option successfully");
+
+driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_invite_1stpage"))).click();
+Thread.sleep(4000);
+
+
+
+driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_invite_1stpage_send"))).click();
+Thread.sleep(4000);
+
+logger.info("Clicked on send button for the Invite more contacts  successfully");
+
+driver.findElement(By.xpath(properties.getProperty("partneracc_m_eve_invite_close"))).click();
+Thread.sleep(4000);
+
+logger.info("Clicked on close button successfully");
+
+
+
+
+	}
+}
