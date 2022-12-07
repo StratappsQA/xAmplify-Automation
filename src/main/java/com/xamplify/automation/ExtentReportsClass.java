58a9e8062fe1ce760a28429394701d019cd36550
Anoj <Anoj@Anoj-sa>
2022-12-07 16:10:53 +0530

    ExtentReportsClass COde Changes Commit.

--- src/main/java/com/xamplify/automation/ExtentReportsClass.java
+++ src/main/java/com/xamplify/automation/ExtentReportsClass.java
@@ -6,8 +6,10 @@ import java.io.File;
 	import org.testng.ITestResult;
 	import org.testng.SkipException;
 	import org.testng.annotations.AfterMethod;
-	import org.testng.annotations.AfterTest;
-	import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 	import org.testng.annotations.Test;
 
 	import com.relevantcodes.extentreports.ExtentReports;
@@ -15,58 +17,61 @@ import java.io.File;
 	import com.relevantcodes.extentreports.LogStatus;
 
 	public class ExtentReportsClass {
-
-		ExtentReports extent;
-		ExtentTest logger;
 		
-		@BeforeTest
-		public void startReport(){
-			
-			extent = new ExtentReports (System.getProperty("user.dir") +"/test-output/STMExtentReport.html", true);
-			extent
-	                .addSystemInfo("Host Name", "Automation")
-	                .addSystemInfo("Environment", "xAmplify")
-	                .addSystemInfo("User Name", "anojkumar");
-			extent.loadConfig(new File(System.getProperty("user.dir")+"/test-output/extent-config.xml"));
-			}
+		static ExtentTest logger;
+		static ExtentReports reports;
 
-		@Test(priority=1,enabled=true)
+		@BeforeSuite
+		public void starttest() {
+			reports = new ExtentReports(System.getProperty("user.dir") + "/test-output/ExtentReportsResult.html", true);
+			
+			reports.addSystemInfo("Host Name", "Automation")
+			.addSystemInfo("Environment", "xAmplify")
+			.addSystemInfo("User Name", "Mounika");
+			//reports.loadConfig(new File(System.getProperty("user.dir")+"/test-output/extent-config.xml"));
+		}
+		
+		@Test
 		public void  OpeningWebApplication(){
-			logger = extent.startTest("passTest");
-			Assert.assertTrue(true);
-			logger.log(LogStatus.PASS, "Test Case Passed is passTest");
 			
-		}
-		
-		@Test(priority=2,enabled=true)
-		public void SignupProcess(){
-			logger = extent.startTest("failTest");
-			Assert.assertTrue(false);
-			logger.log(LogStatus.PASS, "Test Case (failTest) Status is passed");
-		}
-		
-		/*
-		 * @Test public void skipTest(){ logger = extent.startTest("skipTest"); throw
-		 * new SkipException("Skipping - This is not ready for testing "); }
-		 */
-
+			
+			logger = reports.startTest("passTest");
+			/*
+			 * Assert.assertTrue(true); logger.log(LogStatus.PASS, "Test Case Passed");
+			 */
+			}
 		
 		@AfterMethod
-		public void getResult(ITestResult result){
-			if(result.getStatus() == ITestResult.FAILURE){
-				logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getName());
-				logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getThrowable());
-			}else if(result.getStatus() == ITestResult.SKIP){
-				logger.log(LogStatus.SKIP, "Test Case Skipped is "+result.getName());
+		public void getResult(ITestResult result) {
+	if(result.getStatus()==ITestResult.SUCCESS) {
+				
+				logger.log(LogStatus.PASS, "Test Case passed-" +result.getMethod().getMethodName());
+				logger.log(LogStatus.PASS, "Test Case passed-" +result.getTestClass().getName());
+			
 			}
-			extent.endTest(logger);
+			
+
+	else if(result.getStatus()==ITestResult.FAILURE) {
+				
+				logger.log(LogStatus.FAIL, "Test Case failed-" +result.getMethod().getMethodName());
+				logger.log(LogStatus.PASS, "Test Case failed-" +result.getTestClass().getName());
+				logger.log(LogStatus.FAIL, "Test Case failed-" +result.getThrowable());
+			}
+			
+			else if(result.getStatus()==ITestResult.SKIP) {
+				
+				logger.log(LogStatus.FAIL, "Test Case skipped-" +result.getMethod().getMethodName());
+				logger.log(LogStatus.FAIL, "Test Case skipped-" +result.getTestClass().getName());
+				logger.log(LogStatus.FAIL, "Test Case skipped-" +result.getThrowable());
+			}
+		reports.endTest(logger);
 		}
+		@AfterSuite
 		
-		@AfterTest
-		public void endReport(){
-			extent.flush();
-			extent.flush();
+		public void endTest() {
+			
+			reports.flush();
+			reports.close();
 		}
 		
 	}
-
