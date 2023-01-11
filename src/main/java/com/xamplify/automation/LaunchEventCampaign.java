0f3f0580bd311128364850f3457f850df61d6117
Anojkumar Bedadha <banoj@stratapps.com>
2023-01-11 15:56:13 +0530

    Changed the path and misspelled word in testng.xml file.

--- src/main/java/com/xamplify/automation/LaunchEventCampaign.java
+++ src/main/java/com/xamplify/automation/LaunchEventCampaign.java
@@ -14,7 +14,7 @@ public class LaunchEventCampaign extends ExtentReportsClass {
 	WebDriver driver = Instance.getInstance();
 
 	Properties properties = PropertiesFile.readPropertyFile(
-			"D:\\git\\xAmplify-Automation\\src\\main\\resources\\EventCampaign.properties");
+			"D:\\GitHub\\xAmplify-Automation\\src\\main\\resources\\EventCampaign.properties");
 
 	final Logger logger = LogManager.getLogger(LaunchEventCampaign.class);
 
